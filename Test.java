b309f6c8c1161159f18c31b9ab6873a400fcaf17
tetsu8257 <mss_tetsuka@dpt-inc.co.jp>
2026-09-14 11:26:14 +0900

    初めてのコミット

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test{
+    public static void main(String[] arg){
+        System.out.println("こんにちは、これはテストプログラムです。");
+        int x = 10; //整数
+        int y = 20; //整数
+        int total;  //合計を入力
+        total = x * y;
+
+        //合計を出力
+        System.out.println(x+"*"+y+"="+total);
+        System.out.println("このように計算を行い、出力することも可能です。");
+        
+    }
+}
